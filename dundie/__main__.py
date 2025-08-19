f11017d56a4e5d29469de08a29545750de04c729
patlinha <patylinha@yahoo.com.br>
2025-08-19 05:50:45 -0300

    adicionado teste a ser rodado com doctest no arquivo core.py

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,40 +1,5 @@
-import argparse
+from dundie.cli import main
 
-def load(filepath):
-    """Loads data from filepath to the database"""
-    try:
-        with open(filepath) as file_:
-            for line in file_:
-                print(line)
-    except FileNotFoundError as e:
-        print(f"File not found {e}")
-
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="Dunder Mifflin Rewards CLI",
-        epilog="Enjoy and use with cautious",
-    )    
-    parser.add_argument(
-        "subcommand",
-        type=str,
-        help="The subcommand to run",
-        choices=("load", "show", "send"),
-        default="help"
-    )
-    parser.add_argument(
-        "filepath",
-        type=str,
-        help="File path to load",
-        default=None
-    )
-    args = parser.parse_args()
-
-    globals()[args.subcommand](args.filepath)
-
-    
-    print("Executing dundie from entry point.")
 
 if __name__ == "__main__":
     main()
