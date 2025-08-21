d30debb309e9e7df5cf8a09f9d69213876c4ebd6
patlinha <patylinha@yahoo.com.br>
2025-08-21 19:54:40 -0300

    add CI gitHub Actions

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Pati LinA",
+    python_requires=">=3.8",
     packages=find_packages(), #vai retornar todos os pacotes q tenham o __init__ dentro
     entry_points={
         "console_scripts": [
